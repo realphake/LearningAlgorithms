fe81fe767d8791960f386416b1d5e51d0a99bc77
Arjen Swellengrebel <arjen_swellengrebel@msn.com>
2013-10-16 10:16:44 +0200

    Added hidden layer capacity
    Not tested yet!

--- src/NeuralNetwork.java
+++ src/NeuralNetwork.java
@@ -1,36 +1,51 @@
+import java.util.ArrayList;
 import java.util.Random;
 
 public class NeuralNetwork {
-	
+
 	int inAmount;
-	int outAmount;
-	double weights[][];
+	int outAmount[];
+	ArrayList<double[][]> weights;
 	Random randGen;
+
+	public NeuralNetwork( int input, int output ) {
+		this(input, new int[] {output});
+	}
 	
-	public NeuralNetwork( int inputArguments, int outputArguments ) {
+	public NeuralNetwork( int inputArguments, int outputArguments[] ) {
 		inAmount = inputArguments;
 		outAmount = outputArguments;
 		randGen = new Random();
-		weights = new double[inAmount][outAmount];
-		for ( int i = 0; i < inAmount; i++ ) {
-			for ( int o = 0; o < outAmount; o++ ) {
-				weights[i][o] = randGen.nextDouble();
+		weights = new ArrayList<double[][]>();
+		int in = inAmount;
+		for (int n = 0; n < outAmount.length; n++ ){
+			int out = outAmount[n];
+			double[][] element = new double[in][out];
+			for ( int i = 0; i < in; i++ ) {
+				for ( int o = 0; o < out; o++ ) {
+					element[i][o] = randGen.nextDouble();
+				}
 			}
+			weights.add(element);
+			in = out;
 		}
 	}
-	
+
 	@Override
 	public String toString() {
 		String output = "Neural Network with " + inAmount + " inputs";
-		output += " and " + outAmount + " outputs.";
+		output += ", " + (outAmount.length-1) + " hidden layer(s)";
+		output += " and " + outAmount[outAmount.length-1] + " outputs.";
 		return output;
 	}
-	
+
 	public double[] giveOutput( double[] input ) {
-		double[][] inputMatrix = {input};
-		double[] output = Matrix.multiply(inputMatrix, weights)[0];
-		return output;
-		
+		double[][] output = {input};
+		for ( int n = 0; n < weights.size(); n++ ) {
+			output = Matrix.multiply(output, weights.get(n));
+		}
+		return output[0];
+
 	}
 
 }
--- src/TestClass.java
+++ src/TestClass.java
@@ -6,7 +6,10 @@ public class TestClass {
 		double[][] m = {{1,2}};
 		double[][] n = {{3,4},{5,6}};
 		double[][] result = {{13,16}};
+		int[] outputs = {5,2};
 		
+		NeuralNetwork nn = new NeuralNetwork(3,outputs);
+		System.out.println( nn.toString() );
 		if ( Matrix.cols(test) != 3 ) System.err.println("cols don't work");
 		if ( Matrix.rows(test) != 2 ) System.err.println("rows don't work");
 		if ( Matrix.multiply(n,m) != null ) System.err.println("multiply doesn't check size");
