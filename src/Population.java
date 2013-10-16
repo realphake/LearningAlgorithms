7c6a24dff1124d1f64edb64d14334c32ff760371
Arjen <arjen_swellengrebel@msn.com>
2013-10-16 13:44:18 +0200

    more stuff in population

--- src/Population.java
+++ src/Population.java
@@ -5,12 +5,18 @@ public class Population {
 
 	NeuralNetwork denizens[];
 	double fitness[];
+	Random randGen;
 	
 	public Population(int numberOfDenizens) {
 		denizens = new NeuralNetwork[numberOfDenizens];
 		for( int i = 0; i < numberOfDenizens; i++ ) {
 			fitness[i] = 0.0;
 		}
+		randGen = new Random();
+	}
+	
+	public int populationSize() {
+		return denizens.length;
 	}
 	
 	public void giveReward(int denizenID, double reward) {
@@ -18,14 +24,14 @@ public class Population {
 	}
 	
 	public int rouletteSelectDenizen() {
-		double sumFitness = 0.0;
+		double rouletteBall = throwRouletteBall();
+		return whatDenizenDidBallLandOn(rouletteBall);
+	}
+
+	private int whatDenizenDidBallLandOn(double rouletteBall) {
+		double sumFitness = 0;
 		for ( int id = 0; id < fitness.length; id++ ) {
-			sumFitness += fitness[id];
-		}
-		double rouletteBall = new Random().nextDouble()*sumFitness;
-		sumFitness = 0;
-		for ( int id = 0; id < fitness.length; id++ ) {
-			if ( rouletteBall <= fitness[id] + sumFitness ) { //TODO less or equal or just less?
+			if ( rouletteBall <= fitness[id] + sumFitness ) {
 				return id;
 			} else {
 				sumFitness += fitness[id];
@@ -33,9 +39,30 @@ public class Population {
 		}
 		return -1;
 	}
+
+	private double throwRouletteBall() {
+		double sumFitness = 0.0;
+		for ( int id = 0; id < fitness.length; id++ ) {
+			sumFitness += fitness[id];
+		}
+		return randGen.nextDouble()*sumFitness;
+	}
 	
 	public NeuralNetwork returnDenizen( int denizenID ) {
 		return denizens[denizenID];
+	}
+	
+	public double[] askDenizen( int denizenID, double[] question ) {
+		return denizens[denizenID].giveOutput(question);
+	}
+	
+	public void generation() {
+		NeuralNetwork newDenizens[] = denizens;
+		for ( int id = 0; id < newDenizens.length; id++ ) {
+			// TODO this needs to mutate and crossover!
+			newDenizens[id] = denizens[rouletteSelectDenizen()];
+		}
+		denizens = newDenizens;
 	}
 
 }
